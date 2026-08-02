86a37de5b77b16432744ad06836fc3e824b0e6f2
Bohuslav Semenov <bohuslav.semenov@gmail.com>
2026-08-02 23:57:46 +0200

    Add base set of arithmetic, load/store, and jump instructions with tests

--- pkg/cpu/cpu.go
+++ pkg/cpu/cpu.go
@@ -9,6 +9,23 @@ import (
 
 const MemorySize = 19683 // 3^9
 
+// Opcode constants
+const (
+	OpHalt = 0
+	OpAdd  = 1
+	OpSub  = -1
+	OpMul  = 2
+	OpCmp  = -2
+	OpLd   = 3
+	OpSt   = -3
+	OpLdi  = 4
+	OpJmp  = 5
+	OpJeq  = 6
+	OpJne  = -6
+	OpJlt  = 7
+	OpJgt  = -7
+)
+
 type CPU struct {
 	R       [9]tryte.Tryte
 	PC      tryte.Tryte
@@ -58,4 +75,109 @@ func (cpu *CPU) WriteMem(addr tryte.Tryte, val tryte.Tryte) error {
 	}
 	cpu.Memory[idx] = val
 	return nil
+}
+
+func (cpu *CPU) getRegIndex(t tryte.Tryte) int {
+	val := int(t[0]) + int(t[1])*3 + int(t[2])*9
+	return (val%9 + 9) % 9
+}
+
+// Step executes a single instruction cycle (Fetch, Decode, Execute).
+// Returns (keepRunning, error).
+func (cpu *CPU) Step() (bool, error) {
+	// 1. Fetch
+	inst, err := cpu.ReadMem(cpu.PC)
+	if err != nil {
+		return false, fmt.Errorf("fetch error: %w", err)
+	}
+
+	// Increment PC using ALU
+	nextPC, _ := cpu.ALU.Add(cpu.PC, tryte.FromInt(1))
+	cpu.PC = nextPC
+
+	// 2. Decode
+	// Symmetrical 3-3-3 trit extraction
+	srcPart := tryte.Tryte{inst[0], inst[1], inst[2]}
+	dstPart := tryte.Tryte{inst[3], inst[4], inst[5]}
+	opPart  := tryte.Tryte{inst[6], inst[7], inst[8]}
+
+	op := opPart.ToInt()
+	srcIdx := cpu.getRegIndex(srcPart)
+	dstIdx := cpu.getRegIndex(dstPart)
+
+	// 3. Execute
+	switch op {
+	case OpHalt:
+		return false, nil
+
+	case OpAdd: // ADD: R[dst] = R[dst] + R[src]
+		res, carry := cpu.ALU.Add(cpu.R[dstIdx], cpu.R[srcIdx])
+		cpu.R[dstIdx] = res
+		cpu.Carry = carry
+
+	case OpSub: // SUB: R[dst] = R[dst] - R[src]
+		res, carry := cpu.ALU.Sub(cpu.R[dstIdx], cpu.R[srcIdx])
+		cpu.R[dstIdx] = res
+		cpu.Carry = carry
+
+	case OpMul: // MUL: R[dst] = R[dst] * R[src]
+		res, overflow := cpu.ALU.Multiply(cpu.R[dstIdx], cpu.R[srcIdx])
+		cpu.R[dstIdx] = res
+		cpu.Carry = overflow
+
+	case OpCmp: // CMP: compare R[dst] and R[src]
+		cmpResult := cpu.ALU.Compare(cpu.R[dstIdx], cpu.R[srcIdx])
+		cpu.Compare = cmpResult
+
+	case OpLd: // LD: load from address stored in R[src] to R[dst]
+		val, err := cpu.ReadMem(cpu.R[srcIdx])
+		if err != nil {
+			return false, fmt.Errorf("load memory error: %w", err)
+		}
+		cpu.R[dstIdx] = val
+
+	case OpSt: // ST: store value in R[dst] to address stored in R[src]
+		err := cpu.WriteMem(cpu.R[srcIdx], cpu.R[dstIdx])
+		if err != nil {
+			return false, fmt.Errorf("store memory error: %w", err)
+		}
+
+	case OpLdi: // LDI: load immediate value from next PC location
+		val, err := cpu.ReadMem(cpu.PC)
+		if err != nil {
+			return false, fmt.Errorf("load immediate fetch error: %w", err)
+		}
+		cpu.R[dstIdx] = val
+		// Increment PC again since we consumed the immediate
+		nextPC, _ := cpu.ALU.Add(cpu.PC, tryte.FromInt(1))
+		cpu.PC = nextPC
+
+	case OpJmp: // JMP: PC = R[src]
+		cpu.PC = cpu.R[srcIdx]
+
+	case OpJeq: // JEQ: jump if Compare == O
+		if cpu.Compare == tryte.O {
+			cpu.PC = cpu.R[srcIdx]
+		}
+
+	case OpJne: // JNE: jump if Compare != O
+		if cpu.Compare != tryte.O {
+			cpu.PC = cpu.R[srcIdx]
+		}
+
+	case OpJlt: // JLT: jump if Compare == F (<)
+		if cpu.Compare == tryte.F {
+			cpu.PC = cpu.R[srcIdx]
+		}
+
+	case OpJgt: // JGT: jump if Compare == T (>)
+		if cpu.Compare == tryte.T {
+			cpu.PC = cpu.R[srcIdx]
+		}
+
+	default:
+		return false, fmt.Errorf("unknown opcode: %d", op)
+	}
+
+	return true, nil
 }
--- pkg/cpu/cpu_test.go
+++ pkg/cpu/cpu_test.go
@@ -6,6 +6,11 @@ import (
 	"github.com/bohuslavsemenov/tcpu/pkg/tryte"
 )
 
+// Helper to construct a 9-trit instruction from (opcode, src, dst) values using base-3 arithmetic
+func MakeInst(opVal, srcVal, dstVal int) tryte.Tryte {
+	return tryte.FromInt(srcVal + dstVal*27 + opVal*729)
+}
+
 func TestCPU_ResetAndMemory(t *testing.T) {
 	cpu := &CPU{}
 	cpu.Reset()
@@ -31,5 +36,83 @@ func TestCPU_ResetAndMemory(t *testing.T) {
 
 	if readVal.ToInt() != 123 {
 		t.Errorf("expected read value to be 123, got %d", readVal.ToInt())
+	}
+}
+
+func TestCPU_JumpsAndCompare(t *testing.T) {
+	cpu := &CPU{}
+	cpu.Reset()
+
+	// Load 50 into R[4], 50 into R[5], 60 into R[6]
+	cpu.R[4] = tryte.FromInt(50)
+	cpu.R[5] = tryte.FromInt(50)
+	cpu.R[6] = tryte.FromInt(60)
+
+	// Set register target jump addresses
+	cpu.R[7] = tryte.FromInt(100) // Jump destination
+
+	// CMP R[4], R[5] (50 == 50 -> Compare flag = O)
+	_ = cpu.WriteMem(tryte.FromInt(0), MakeInst(OpCmp, 5, 4))
+	// JEQ R[7] (should jump to 100)
+	_ = cpu.WriteMem(tryte.FromInt(1), MakeInst(OpJeq, 7, 0))
+
+	// Step 1: execute CMP
+	_, _ = cpu.Step()
+	if cpu.Compare != tryte.O {
+		t.Errorf("expected Compare flag to be O (equal), got %v", cpu.Compare)
+	}
+
+	// Step 2: execute JEQ (should branch to PC=100)
+	_, _ = cpu.Step()
+	if cpu.PC.ToInt() != 100 {
+		t.Errorf("expected PC to jump to 100, got %d", cpu.PC.ToInt())
+	}
+
+	// Reset PC to 10
+	cpu.PC = tryte.FromInt(10)
+	// CMP R[4], R[6] (50 < 60 -> Compare flag = F)
+	_ = cpu.WriteMem(tryte.FromInt(10), MakeInst(OpCmp, 6, 4))
+	// JLT R[7] (should jump to 100)
+	_ = cpu.WriteMem(tryte.FromInt(11), MakeInst(OpJlt, 7, 0))
+
+	// Step CMP
+	_, _ = cpu.Step()
+	if cpu.Compare != tryte.F {
+		t.Errorf("expected Compare flag to be F (less), got %v", cpu.Compare)
+	}
+
+	// Step JLT
+	_, _ = cpu.Step()
+	if cpu.PC.ToInt() != 100 {
+		t.Errorf("expected PC to jump to 100, got %d", cpu.PC.ToInt())
+	}
+}
+
+func TestCPU_StoreAndLoad(t *testing.T) {
+	cpu := &CPU{}
+	cpu.Reset()
+
+	// We want to store the value of R[0] (which is 99) at address 500
+	cpu.R[0] = tryte.FromInt(99)
+	cpu.R[1] = tryte.FromInt(500) // Address register
+
+	// ST: store R[0] to address in R[1]
+	_ = cpu.WriteMem(tryte.FromInt(0), MakeInst(OpSt, 1, 0))
+	// LD: load address in R[1] to R[2]
+	_ = cpu.WriteMem(tryte.FromInt(1), MakeInst(OpLd, 1, 2))
+	// HALT
+	_ = cpu.WriteMem(tryte.FromInt(2), MakeInst(OpHalt, 0, 0))
+
+	// ST
+	_, _ = cpu.Step()
+	val, _ := cpu.ReadMem(cpu.R[1])
+	if val.ToInt() != 99 {
+		t.Errorf("expected value at address 500 to be 99, got %d", val.ToInt())
+	}
+
+	// LD
+	_, _ = cpu.Step()
+	if cpu.R[2].ToInt() != 99 {
+		t.Errorf("expected R[2] to load 99, got %d", cpu.R[2].ToInt())
 	}
 }
